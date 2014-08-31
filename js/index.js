720f913eca606ca00ae212d008a5555bf5504e6a
svenlang <svenlang07@gmail.com>
2014-08-31 15:25:25 +0200

    add for and backward date

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 function createCalendar(id) {
     id = _.isNumber(id) && id > 0 ? id : 1;
-    moment.locale("de");
+    // todo : date is 1 day in future
 
     var output = "",
         i = 0;
@@ -18,28 +18,30 @@ function createCalendar(id) {
             '</tr>',
                 // todo here start firstDayWeekDay
             '<tr>',
-                '<% if (firstDayWeekDay > 0) { %>',
-                    '<td colspan="<%= firstDayWeekDay %>">&nbsp;</td>',
+                '<% for (var p = lastMonthDays - firstDayWeekDay; p < lastMonthDays; p++) { %>',
+                    '<td class="OverMonth"><%= p + 1 %></td>',
                 '<% } %>',
                 '<% for (var k = 0; k < daysInMonth; k++) { %>',
                     '<% if (firstDayWeekDay++ % 7 == 0) { %>', // todo || firstDayWeekDay == 0, then have double TR
                         '<tr>',
                     '<% } %>',
-                    '<td id="date_<%= year_month_year(k) %>"><%= k + 1 %></td>',
+                    '<td id="<%= year_month_day(k) %>"><%= k + 1 %></td>',
                 '<% } %>',
-                '<% if (firstDayWeekDay % 7 > 0) { %>',
-                    '<td colspan="<%= 7 - (firstDayWeekDay % 7) %>">&nbsp;</td>',
+                '<% for (var m = 0; m < 7 - (firstDayWeekDay % 7); m++) { %>',
+                    '<td class="OverMonth"><%= m + 1 %></td>',
                 '<% } %>',
             '</tr>'
         ].join(""),
         {
-            year_month_year: function(k) {
-                return moment().add(i, "month").startOf("month").add(k, "day").format("YYYY_MM_DD");
+            lastMonthDays: moment().add(i - 1, "month").endOf('month').format("D"),
+            year_month_day: function(k) {
+                return "date_" + moment().add(i, "month").startOf("month").add(k, "day").format("YYYY_MM_DD");
             },
             monthName: moment().add(i, "month").format("MMMM YYYY"),
             // todo with global momentJS weeksnames
             weekName: ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
-            firstDayWeekDay: moment().add(i, "month").subtract(moment().add(i, "month").format("D"), 'days').day(),
+            //firstDayWeekDay: moment().add(i, "month").subtract(moment().add(i, "month").format("D"), 'days').day(),
+            firstDayWeekDay: moment().add(i, "month").startOf("month").day(),
             daysInMonth: moment().add(i, "month").endOf('month').format("D")
         });
 
