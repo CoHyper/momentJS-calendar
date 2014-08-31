654a3db5e53f285e7b25644fb136a4f2d443c7ba
svenlang <svenlang07@gmail.com>
2014-08-31 14:57:15 +0200

    add each date as id

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 
 function createCalendar(id) {
     id = _.isNumber(id) && id > 0 ? id : 1;
+    moment.locale("de");
 
     var output = "",
         i = 0;
 
     for (i; i < id; i++) {
-        output += 
-        //_.bind( // todo check
-            _.template([
+        output += _.template([
             '<tr>',
                 '<th colspan="7"><%= monthName %></th>',
             '</tr>',
@@ -26,7 +25,7 @@ function createCalendar(id) {
                     '<% if (firstDayWeekDay++ % 7 == 0) { %>', // todo || firstDayWeekDay == 0, then have double TR
                         '<tr>',
                     '<% } %>',
-                    '<td align="center"><%= (k + 1) %></td>',
+                    '<td id="date_<%= year_month_year(k) %>"><%= k + 1 %></td>',
                 '<% } %>',
                 '<% if (firstDayWeekDay % 7 > 0) { %>',
                     '<td colspan="<%= 7 - (firstDayWeekDay % 7) %>">&nbsp;</td>',
@@ -34,14 +33,15 @@ function createCalendar(id) {
             '</tr>'
         ].join(""),
         {
+            year_month_year: function(k) {
+                return moment().add(i, "month").startOf("month").add(k, "day").format("YYYY_MM_DD");
+            },
             monthName: moment().add(i, "month").format("MMMM YYYY"),
             // todo with global momentJS weeksnames
             weekName: ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
             firstDayWeekDay: moment().add(i, "month").subtract(moment().add(i, "month").format("D"), 'days').day(),
             daysInMonth: moment().add(i, "month").endOf('month').format("D")
-        }
-        //), i
-        );
+        });
 
     } // end for()
 
